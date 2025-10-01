0b19b9d4098f7844e74f8f35c0c5bf6782ed7a05
Kees Kuip <keeskuip@cubeautomatisering.nl>
2025-10-01 17:15:26 +0200

    Enable register converters

--- src/main/java/org/kku/fx/util/Converters.java
+++ src/main/java/org/kku/fx/util/Converters.java
@@ -2,6 +2,7 @@ package org.kku.fx.util;
 
 import org.kku.fx.ui.util.ColorPalette;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class Converters
   extends org.kku.common.util.Converters
@@ -10,4 +11,10 @@ public class Converters
   {
     return new Converter<Color>(Color::web, ColorPalette::toHexString);
   }
+
+  public static Converter<Font> getFontConverter()
+  {
+    return new Converter<Font>(fontName -> null, font -> font.getName() + "-" + font.getSize());
+  }
+
 }
